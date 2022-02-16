0d1bbe8c06137344ec5361e41344b37b7215254f
HighEyeCue <aarontcu@gmail.com>
2022-02-16 17:49:44 -0500

    added methods

--- scanner.py
+++ scanner.py
@@ -1,4 +1,13 @@
-file = open(input("enter file name:\n"))
+file = None
 
-for line in file:
-    print(line)
+def openFile():
+    global file 
+    file = open(input("enter file name:\n"))
+
+def printFile():
+    global file
+    for line in file:
+        print(line)
+
+openFile()
+printFile()
